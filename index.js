3c7f2095512ab3b22007d46dbee3fdba477533b1
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2018-01-27 20:02:39 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * @see https://stackoverflow.com/questions/40812254/how-to-get-a-random-image-from-the-images-directory-in-an-express-environment
+ */
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    fs = require('fs');
+
+server.listen(3000);
+
+app.get('dist/', function (req, res) {
+    res.sendFile(__dirname + 'index.html');
+});
+
+app.use(express.static('dist'));
+
+function getRandFile() {
+    const imageDir = '../public/images/';
+    fs.readdir(imageDir, (err, files) => {
+        files.forEach(file => {
+            //will add file name to an array and then return a random file name
+        });
+    });
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Random Images
+
+Reads random images from a list of directories.
+Creates a composition on canvas of combined image clips.
