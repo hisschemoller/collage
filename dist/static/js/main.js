d7b8624a7494435e5ee26d193d1922fd6e397abd
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2018-02-02 13:55:25 +0100

    Preload all images.

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(e) {
     let config,
         canvas,
-        ctx,
-        images;
+        ctx;
 
     const init = () => {
         loadJSON('json?type=config').then(result => {
@@ -17,52 +16,19 @@ document.addEventListener('DOMContentLoaded', function(e) {
     };
 
     const createCollage = () => {
-        images = [];
         const numImages = 3;
         loadJSON(`json?type=image&amount=${numImages}`).then(data => {
-            loadAllImages(data, images).then(() => {
-                setTimeout(function() {
-                    for (let i = 0, n = images.length; i < n; i++) {
-                        let img = images[i].image,
-                            sWidth = 100 + Math.random() * (img.width - 100),
-                            sHeight = 100 + Math.random() * (img.height - 100),
-                            sx = Math.random() * (img.width - sWidth),
-                            sy = Math.random() * (img.height - sHeight),
-                            scale = Math.random() * 1.4,
-                            dWidth = sWidth * scale,
-                            dHeight = sHeight * scale,
-                            dx = Math.random() * (canvas.width - dWidth),
-                            dy = Math.random() * (canvas.height - dHeight);
-                        // console.log(sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-                        ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-                    }
-                }, 200);
-            });
+            console.log(data);
+            Promise.all(data.map(loadImage)).then(drawAll);
         });
     };
 
-    const loadAllImages = (imageData, images) => {
-        return new Promise((resolve, reject) => {
-            let promise = loadImage(imageData, images, 0);
-            for (let i = 1, n = imageData.length; i < n; i++) {
-                promise = promise.then(() => { loadImage(imageData, images, i) });
-            }
-            promise.then(() => { resolve() });
-        });
-    };
-
-    const loadImage = (imageData, images, index) => {
-        return new Promise((resolve, reject) => {
+    const loadImage = imageData => {
+        return new Promise(resolve => {
             const img = new Image();
-            img.src = `image?dir=${imageData[index].dir}&img=${imageData[index].image}`;
-            img.onload = () => { 
-                images.push({
-                    image: img,
-                    filename: imageData[index].image,
-                    filepath: imageData[index].dir
-                })
-                resolve();
-            };
+            img.onload = () => resolve(img);
+            img.onerror = () => resolve(null);
+            img.src = `image?dir=${imageData.dir}&img=${imageData.image}`;
         });
     };
     
@@ -78,6 +44,23 @@ document.addEventListener('DOMContentLoaded', function(e) {
             };
             xobj.send(null);  
         });
+    };
+    
+    const drawAll = images => {
+        for (let i = 0, n = images.length; i < n; i++) {
+            let img = images[i],
+                sWidth = 100 + Math.random() * (img.width - 100),
+                sHeight = 100 + Math.random() * (img.height - 100),
+                sx = Math.random() * (img.width - sWidth),
+                sy = Math.random() * (img.height - sHeight),
+                scale = Math.random() * 1.4,
+                dWidth = sWidth * scale,
+                dHeight = sHeight * scale,
+                dx = Math.random() * (canvas.width - dWidth),
+                dy = Math.random() * (canvas.height - dHeight);
+            // console.log(sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+            ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+        }
     };
     
     init();
