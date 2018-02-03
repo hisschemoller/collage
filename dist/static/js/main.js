68a7e44aea06cffa5a93aeb03b08ec29cc2031ac
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2018-02-03 22:57:30 +0100

    Three vertically centered images.

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -52,55 +52,52 @@ document.addEventListener('DOMContentLoaded', function(e) {
     
     const drawAll = images => {
         drawBackground(images[0]);
-        // drawMidDistance(images[1]);
-        // drawCloseDistance(images[2]);
+        drawMidDistance(images[1]);
+        drawCloseDistance(images[2]);
     };
 
     const drawBackground = img => {
         let sWidth = 100 + (Math.random() * (img.width - 100)),
-            sHeight = (canvas.height / canvas.width) * sWidth; // 100 + (Math.random() * (img.height - 100)),
+            sHeight = (canvas.height / canvas.width) * sWidth,
             sx = Math.random() * (img.width - sWidth),
-            sy = Math.random() * (img.height - sHeight),
+            sy = (img.height / 2) - (sHeight / 2),
             scale = Math.max(1, Math.max(canvas.width / sWidth, canvas.height / sHeight)),
             dWidth = sWidth * scale,
             dHeight = sHeight * scale,
             dx = Math.random() * (canvas.width - dWidth),
             dy = (canvas.height / 2) - (dHeight / 2);
-        console.log(Math.round(sx), Math.round(sy), Math.round(sWidth), Math.round(sHeight), Math.round(dx), Math.round(dy), Math.round(dWidth), Math.round(dHeight), scale);
-        console.log((sHeight / sWidth).toFixed(3), (dHeight / dWidth).toFixed(3));
+        // console.log(Math.round(sx), Math.round(sy), Math.round(sWidth), Math.round(sHeight), Math.round(dx), Math.round(dy), Math.round(dWidth), Math.round(dHeight), scale);
+        // console.log((sHeight / sWidth).toFixed(3), (dHeight / dWidth).toFixed(3));
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
     };
 
     const drawMidDistance = img => {
         let sWidth = 100 + Math.random() * (img.width - 100),
-            sHeight = 100 + Math.random() * (img.height - 100),
+            sHeight = Math.min(sWidth, img.height),
             sx = Math.random() * (img.width - sWidth),
-            sy = Math.random() * (img.height - sHeight),
+            sy = (img.height / 2) - (sHeight / 2),
             scale = Math.max(1, Math.max(canvas.width / sWidth, canvas.height / sHeight)),
             dWidth = sWidth * scale,
             dHeight = sHeight * scale,
             isLeft = !!Math.round(Math.random()),
             xPosition = (canvas.width / 4) + (Math.random() * (canvas.width / 2))
             dx = isLeft ? xPosition - dWidth : xPosition,
-            dy = 0;
+            dy = (canvas.height / 2) - (dHeight / 2);
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
     }
     
     const drawCloseDistance = img => {
         let sWidth = 100 + Math.random() * (img.width - 100),
-            sHeight = 100 + Math.random() * (img.height - 100),
+            sHeight = Math.min(sWidth, img.height),
             sx = Math.random() * (img.width - sWidth),
-            sy = Math.random() * (img.height - sHeight),
+            sy = (img.height / 2) - (sHeight / 2),
             scale = Math.max(1, Math.max(canvas.width / sWidth, canvas.height / sHeight)),
             dWidth = sWidth * scale,
             dHeight = sHeight * scale,
             isLeft = !!Math.round(Math.random()),
-            isTop = !!Math.round(Math.random()),
-            xPosition = (canvas.width / 4) + (Math.random() * (canvas.width / 2)),
-            yPosition = (canvas.height / 4) + (Math.random() * (canvas.height / 2)),
-            dx = isLeft ? xPosition - dWidth : xPosition,
-            dy = isTop ? yPosition - dHeight : yPosition;
-        // console.log(sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+            xPosition = (canvas.width * 0.7) + (Math.random() * (canvas.width * 0.3))
+            dx = isLeft ? canvas.width - xPosition - dWidth : xPosition,
+            dy = (canvas.height / 2) - (dHeight / 2);
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
     }
     
