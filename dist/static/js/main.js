f857aabdc097c793c0976c000f4e1f9c7f3927c6
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2018-02-17 22:04:11 +0100

    Direct rotate and draw of rotated iPhone image.

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -22,10 +22,9 @@ document.addEventListener('DOMContentLoaded', function(e) {
     };
 
     const createCollage = () => {
-        const numImages = 1;
+        const numImages = 3;
         loadJSON(`json?type=image&amount=${numImages}`).then(data => {
             console.log(data);
-            // Promise.all(data.map(loadImage)).then(drawAll);
             Promise.all(data.map(loadImage)).then(images => {
                 Promise.all(images.map(getIPhoneRotated)).then(drawAll);
             });
@@ -93,28 +92,30 @@ document.addEventListener('DOMContentLoaded', function(e) {
     const getIPhoneRotated = (img) => {
         return new Promise((resolve, reject) => {
             if (!img) {
-                resolve({ img, isrotated: false });
+                resolve({ img, isRotated: false });
             }
             EXIF.getData(img, function() {
                 const make = EXIF.getTag(this, 'Make');
                 if (make && make.indexOf('Apple') > -1 && EXIF.getTag(this, 'Orientation') === 6) {
-                    console.log('getIPhoneRotated', img.src);
-                    resolve({ img, isrotated: true });
+                    console.log('This is a rotated iPhone photo', img.src);
+                    resolve({ img, isRotated: true });
                 } else {
-                    resolve({ img, isrotated: false });
+                    resolve({ img, isRotated: false });
                 }
             });
         });
     };
     
     const drawAll = (imageData) => {
-        console.log(arguments.length, arguments, imageData[0]);
         let count = 0;
         imageData.forEach(data => {
             if (data.img) {
                 switch (count) {
                     case 0:
-                        test(data.img, data.isRotated); // drawBackground(data.img, data.isRotated);
+                        // test(data.img, data.isRotated);
+                        drawBackground(data.img, data.isRotated);
+                        // drawMidDistance(data.img, data.isRotated);
+                        // drawCloseDistance(data.img, data.isRotated);
                         break;
                     case 1:
                         drawMidDistance(data.img, data.isRotated);
@@ -129,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
     };
 
     const test = (img, isRotated) => {
-        const sx = 100,
+        let sx = 100,
             sy = 200,
             sWidth = 800,
             sHeight = 600,
@@ -142,24 +143,26 @@ document.addEventListener('DOMContentLoaded', function(e) {
         ctx.beginPath();
         ctx.moveTo(dx, dy);
         ctx.bezierCurveTo(dx + (sWidth * strength), dy, dx + dWidth, dy+ dHeight - (dHeight * strength), dx + dWidth, dy+ dHeight);
-        ctx.lineTo(dx, dy+ dHeight);
+        ctx.lineTo(dx, dy + dHeight);
         ctx.lineTo(dx, dy);
         ctx.closePath();
         ctx.clip();
+        
+        if (isRotated) {
+            ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = rotateCoordinates(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight));
+        }     
 
         ctx.save();
         ctx.translate(0, 0);
         ctx.rotate(Math.PI / 2);
-        ctx.drawImage(img, sy, img.height - sWidth - sx, sHeight, sWidth, dy, 0 - dWidth - dx, dHeight, dWidth);
+        ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
 
-        // ctx.rect(0, -400, 400, 400);
         ctx.lineWidth = 8;
         ctx.strokeStyle = '#ff0000';
         ctx.stroke();
         ctx.restore();
     };
 
-    const drawBackground = img => {
     const rotateCoordinates = (img, _sx, _sy, _sWidth, _sHeight, _dx, _dy, _dWidth, _dHeight) => {
         return {
             sx: _sy,
@@ -172,48 +175,98 @@ document.addEventListener('DOMContentLoaded', function(e) {
             dHeight: _dWidth
         };
     };
-        let sWidth = 100 + (Math.random() * (img.width - 100)),
+
+    const drawBackground = (img, isRotated) => {
+        const imgWidth = isRotated ? img.height : img.width,
+            imgHeight = isRotated ? img.width : img.height;
+
+        let sWidth = 100 + (Math.random() * (imgWidth - 100)),
             sHeight = (canvas.height / canvas.width) * sWidth,
-            sx = Math.random() * (img.width - sWidth),
-            sy = (img.height / 2) - (sHeight / 2),
+            sx = Math.random() * (imgWidth - sWidth),
+            sy = (imgHeight / 2) - (sHeight / 2),
             scale = Math.max(1, Math.max(canvas.width / sWidth, canvas.height / sHeight)),
             dWidth = sWidth * scale,
             dHeight = sHeight * scale,
             dx = Math.random() * (canvas.width - dWidth),
             dy = (canvas.height / 2) - (dHeight / 2);
+        
+        if (isRotated) {
+            ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = rotateCoordinates(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight));
+        }
+
+        ctx.save();
+
+        if (isRotated) {
+            ctx.rotate(Math.PI / 2);
+        }
+        
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
+        ctx.restore();
     };
 
-    const drawMidDistance = img => {
+    const drawMidDistance = (img, isRotated) => {
+        const imgWidth = isRotated ? img.height : img.width,
+            imgHeight = isRotated ? img.width : img.height;
+
         const clipWidth = (canvas.width * 0.2) + (Math.random() * (canvas.width * 0.6)),
             clipHeight = canvas.height,
             isLeft = !!Math.round(Math.random());
+
+        let {sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = processImage(imgWidth, imgHeight, clipWidth, clipHeight, isLeft);
         
         createPath(clipWidth, clipHeight, isLeft, 0.7);
-        ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = processImage(img, clipWidth, clipHeight, isLeft));
+
         ctx.save();
         ctx.clip();
+
+        if (isRotated) {
+            ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = rotateCoordinates(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight));
+        }
+
+        if (isRotated) {
+            ctx.rotate(Math.PI / 2);
+        }
+        
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-        ctx.restore();
+        
         // ctx.lineWidth = 8;
         // ctx.strokeStyle = '#ff0000';
         // ctx.stroke();
+
+        ctx.restore();
     }
     
-    const drawCloseDistance = img => {
+    const drawCloseDistance = (img, isRotated) => {
+        const imgWidth = isRotated ? img.height : img.width,
+            imgHeight = isRotated ? img.width : img.height;
+
         const clipWidth = (canvas.width * 0.2) + (Math.random() * (canvas.width * 0.5)),
             clipHeight = canvas.height,
             isLeft = !!Math.round(Math.random());
         
+        let {sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = processImage(imgWidth, imgHeight, clipWidth, clipHeight, isLeft);
+        
         createPath(clipWidth, clipHeight, isLeft, 0.9);
-        ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = processImage(img, clipWidth, clipHeight, isLeft));
+
         ctx.save();
         ctx.clip();
+
+        if (isRotated) {
+            ({sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight} = rotateCoordinates(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight));
+        }
+
+        if (isRotated) {
+            ctx.rotate(Math.PI / 2);
+        }
+        
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-        ctx.restore();
+        
         // ctx.lineWidth = 8;
         // ctx.strokeStyle = '#00ff00';
         // ctx.stroke();
+
+        ctx.restore();
     };
 
 
@@ -294,11 +347,11 @@ document.addEventListener('DOMContentLoaded', function(e) {
         ctx.closePath();
     };
 
-    const processImage = (img, clipWidth, clipHeight, isLeft) => {
-        const sWidth = 100 + Math.random() * (img.width - 100),
-            sHeight = Math.min(sWidth, img.height),
-            sx = Math.random() * (img.width - sWidth),
-            sy = (img.height / 2) - (sHeight / 2),
+    const processImage = (imgWidth, imgHeight, clipWidth, clipHeight, isLeft) => {
+        const sWidth = 100 + Math.random() * (imgWidth - 100),
+            sHeight = Math.min(sWidth, imgHeight),
+            sx = Math.random() * (imgWidth - sWidth),
+            sy = (imgHeight / 2) - (sHeight / 2),
             scale = Math.max(1, Math.max(clipWidth / sWidth, clipHeight / sHeight)),
             dWidth = sWidth * scale,
             dHeight = sHeight * scale,
